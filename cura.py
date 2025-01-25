234e2147de3fdb9b9e60bcdefd5429d69f2484dc
SaaimahSiraj <168414548+SaaimahSiraj@users.noreply.github.com>
2025-01-25 12:09:32 +0530

    Add files via upload

---
+++ cura.py
@@ -0,0 +1,265 @@
+import os
+import json
+import requests
+import streamlit as st
+from streamlit_option_menu import option_menu
+from streamlit_lottie import st_lottie
+from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+from langchain.memory import ConversationBufferMemory
+from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
+from langchain.chains.question_answering import load_qa_chain
+from langchain.vectorstores import FAISS
+from dotenv import load_dotenv
+import google.generativeai as genai
+
+load_dotenv()
+os.environ['GOOGLE_API_KEY'] = os.getenv('GOOGLE_API_KEY')
+
+genai.configure(api_key=os.getenv('GOOGLE_API_KEY'))
+
+def load_lottieurl(url: str):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
+
+# Consultation functions
+def get_diagnosis_chain():
+    prompt_template = """
+    Given the following symptoms: {symptoms}, provide a possible diagnosis and suggest prevention measures.
+    Ensure the response is clear, concise, and considers all listed symptoms.
+    """
+    model = ChatGoogleGenerativeAI(model="gemini-pro", client=genai, temperature=0.5)
+    prompt = PromptTemplate(template=prompt_template, input_variables=["symptoms"])
+    chain = LLMChain(llm=model, prompt=prompt, output_key='diagnosis', memory=ConversationBufferMemory())
+    return chain
+
+def clear_chat_history():
+    st.session_state.messages = [{"role": "assistant", "content": "How can I assist with your symptoms today?"}]
+    st.session_state.symptoms = []
+
+def process_input(user_input):
+    greetings = ["hey", "hello", "helloo"]
+    symptom_queries = ["Could you help me analyze my symptoms", "I need a diagnosis", "symptom analysis", "what's wrong with me", "diagnosis"]
+    user_input_lower = user_input.lower()
+
+    if any(greet in user_input_lower for greet in greetings):
+        response = "Hello! How can I assist you today?"
+
+    elif any(query in user_input_lower for query in symptom_queries):
+        if st.session_state.symptoms:
+            symptoms = ','.join(sorted(set(st.session_state.symptoms)))
+            chain = get_diagnosis_chain()
+            response = chain.run({"symptoms": symptoms})
+
+            if len(response.split()) < 20 or response.count('diagnosis') > 1:
+                response += "The response may be incomplete or repetitive. Please provide more detailed symptoms or clarify the current ones."
+            st.session_state.symptoms = []
+        else:
+            response = "I can help with diagnosing your symptoms. Please list all your symptoms so I can assist you effectively. Prompt (done) after you are done listing your symptoms."
+
+    elif user_input_lower == 'done':
+        if st.session_state.symptoms:
+            symptoms = ','.join(sorted(set(st.session_state.symptoms)))
+            chain = get_diagnosis_chain()
+            response = chain.run({"symptoms": symptoms})
+
+            if len(response.split()) < 20 or response.count('diagnosis') > 1:
+                response += " The response may be incomplete or repetitive. Please provide more detailed symptoms or clarify the current ones."
+
+            st.session_state.symptoms = []  # Clear symptoms after diagnosis
+        else:
+            response = "You haven't listed any symptoms. Please list your symptoms before typing 'done'."
+
+    else:
+        st.session_state.symptoms.append(user_input)
+        response = f"Thank you for sharing. Current symptoms: {', '.join(st.session_state.symptoms)}"
+
+    return response
+
+# Medical record reader
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdfreader = PdfReader(pdf)
+        for page in pdfreader.pages:
+            text += page.extract_text()
+    return text
+
+def get_text_chunks(text):
+    text_split = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
+    chunks = text_split.split_text(text)
+    return chunks
+
+def get_vector_store(text_chunks):
+    embeddings = GoogleGenerativeAIEmbeddings(model="embedding-001")
+    vector_store = FAISS.from_texts(text_chunks, embeddings)
+    vector_store.save_local("faiss_index")
+
+def conversational_chain():
+    prom_template = """
+    You are a medical records assistant. Analyze the following medical records and answer the questions as accurately as possible.
+    Ensure your response includes all the relevant medical details and consider all the documents provided.
+    If the answer isn't in the context, provide the closest relevant information based on the data provided.\n\n
+    Context:\n{context}?\n
+    Question:\n{question}?\n
+    
+    Answer:
+    """
+    model = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.3)
+    prompt = PromptTemplate(template=prom_template, input_variables=["context", "question"])
+    chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
+    return chain
+
+def process_user_question(user_question):
+    embeddings = GoogleGenerativeAIEmbeddings(model="embedding-001")
+    new_db = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True)
+    docs = new_db.similarity_search(user_question)
+    chain = conversational_chain()
+
+    response = chain(
+        {"input_documents": docs, "question": user_question},
+        return_only_outputs=True
+    )
+    return response
+
+# Main function
+def main():
+    st.set_page_config(page_title="Cura(1)", page_icon="🩺")
+
+    with st.sidebar:
+        selected = option_menu(
+            "Trail", ["Landing Page", "Consultation", "Medical Record Reader", "Nearest Doctors"],
+            icons=["house", "chat", "file-medical", "map-marker-alt"],
+            menu_icon="cast", default_index=0
+        )
+
+    if selected == "Landing Page":
+        st.title("Welcome to Cura!")
+        st.header("Cura: where health meets innovation")
+        st.header("Empowering your health, one chat at a time")
+        st.write("Our healthcare chatbot is designed to assist you with understanding your symptoms, finding the nearest doctors, and managing your medical records. Whether you're feeling under the weather or just need quick medical advice, we're here to help!")
+
+        st.markdown("""<style>
+        .feature-box {
+            padding: 20px;
+            border-radius: 10px;
+            transition: box-shadow 0.3s ease-in-out;
+            text-align: center;
+            cursor: pointer;
+        }
+        .feature-box:hover {
+            box-shadow: 0 0 15px rgba(0,123,255,0.7);
+        }
+        .center-button {
+            display:flex;
+            justify-content:center;
+            margin-top:40px;
+        }
+        </style>""", unsafe_allow_html=True)
+
+        st.write("----------------------------------------------------------------------------------------------------")
+        st.header("Key Features")
+        col1, col2, col3, col4 = st.columns(4)
+
+        with col1:
+            st.markdown("""
+            <div class="feature-box">
+            <h2>💬 Consultation</h2>
+            <p>Chat with our intelligent bot to describe your symptoms and receive a preliminary diagnosis. 
+            Our AI-driven bot provides personalized insights based on your input.</p>
+            </div>
+            """, unsafe_allow_html=True)
+
+        with col2:
+            st.markdown("""
+            <div class="feature-box">
+            <h2>📄 Medical Record Reader</h2>
+            <p>Easily upload and access your medical records in one place. 
+            <i>(Coming Soon)</i> Our tool will help you understand your medical history and share it with your healthcare providers.</p>
+            </div>
+            """, unsafe_allow_html=True)
+
+        with col3:
+            st.markdown("""
+            <div class="feature-box">
+            <h2>🗺️ Nearest Doctors</h2>
+            <p>Find the nearest doctors and medical facilities with ease. 
+            <i>(Coming Soon)</i> Get directions, contact information, and reviews to make informed decisions about your healthcare.</p>
+            </div>
+            """, unsafe_allow_html=True)
+
+        with col4:
+            lottie_hello = load_lottieurl("https://lottie.host/88319248-8994-431c-a635-aaf2ab675ecd/NQ2nhrJO4e.json")
+            st_lottie(
+                lottie_hello,
+                speed=1,
+                reverse=False,
+                loop=True,
+                quality="high",
+                height=400,
+                width=300,
+                key=None,
+            )
+
+        st.write("")
+        st.write("Ready to take control of your health? Get started by heading to the Consultation section and chatting with our bot now.")
+
+        st.write("---------------------------------------------------------------------------------------------------------------------------------")
+        st.write("If you have any questions or need assistance, feel free to contact us at tejaswimahadev9@gmail.com.")
+        st.write("---------------------------------------------------------------------------------------------------------------------------------")
+        st.write("**Disclaimer**: This chatbot is not a substitute for professional medical advice, diagnosis, or treatment. Always seek the advice of your physician or other qualified healthcare providers with any questions you may have regarding a medical condition.")
+    
+    elif selected == "Consultation":
+        st.title("Chat with Cura")
+        st.write("Describe your symptoms, ask for a diagnosis, or simply say hello!")
+
+        if "messages" not in st.session_state:
+            st.session_state.messages = [{"role": "assistant", "content": "How can I assist you with your symptoms today?"}]
+        if "symptoms" not in st.session_state:
+            st.session_state.symptoms = []
+
+        if prompt := st.chat_input():
+            st.session_state.messages.append({"role": "user", "content": prompt})
+            with st.chat_message("user"):
+                st.write(prompt)
+
+            if st.session_state.messages[-1]["role"] == "user":
+                with st.chat_message("assistant"):
+                    with st.spinner("Thinking..."):
+                        response = process_input(prompt)
+                        st.write(response)
+                        st.session_state.messages.append({"role": "assistant", "content": response})
+
+        if st.session_state.symptoms:
+            st.write(f"Current Symptoms: {', '.join(st.session_state.symptoms)}")
+    
+    elif selected == "Medical Record Reader":
+        st.title("Medical Record Reader")
+        st.write("Upload and analyze your medical records. The AI will assist you in understanding the health data")
+
+        pdf_docs = st.file_uploader("Upload your medical records", type=["pdf"], accept_multiple_files=True)
+
+        if pdf_docs:
+            with st.spinner("Processing documents..."):
+                raw_text = get_pdf_text(pdf_docs)
+                text_chunks = get_text_chunks(raw_text)
+                get_vector_store(text_chunks)
+            st.write("Documents processed successfully... You can now ask questions about your medical records")
+            user_question = st.text_input("Enter your query about the records")
+
+            if user_question:
+                with st.spinner("Finding the answer..."):
+                    response = process_user_question(user_question)
+                    st.write(response)
+    
+    elif selected == "Nearest Doctors":
+        st.title("Nearest Doctors")
+        st.write("Find the closest medical facilities and healthcare professionals in your area")
+        st.write("Features coming soon.")
+
+if __name__ == "__main__":
+    main()
